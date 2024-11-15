22cec9ae70fe552d17ed3e13efb7d80778f5c293
Mohamad Abdou <Mohamad-M-Abdou@hotmail.com>
2024-11-16 01:14:42 +0300

    Finishing the database structure and adding the essintial data seed

---
+++ app/Http/Controllers/RequestFlowController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RequestFlowController extends Controller
+{
+    //
+}
---
+++ database/migrations/2024_11_15_212049_create_annual_request_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('annual_request_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('annual_request_id')->constrained()->onDelete('cascade');
+            $table->foreignId('item_id');
+            $table->integer('quantity');
+            $table->boolean('frozen')->default(0);
+            $table->string('freeze_reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('annual_request_items');
+    }
+};
